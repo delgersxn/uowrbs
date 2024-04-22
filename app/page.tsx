4cbe64fb4b70a99684d1e5714b4fc3cafeb47d7f
Delgersaikhan <mcmncl3@gmail.com>
2024-04-23 00:10:57 +0800

    cleaning

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,6 @@ import { createClient } from "@/utils/supabase/server";
 import Navbar from "@/components/Navbar";
 
 export default async function Index() {
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient();
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
-
-  const isSupabaseConnected = canInitSupabaseClient();
-
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <Navbar />
@@ -22,7 +9,6 @@ export default async function Index() {
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <main className="flex-1 flex flex-col gap-6">
           <h2 className="font-bold text-4xl mb-4">Hello</h2>
-          {/* {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />} */}
         </main>
       </div>
     </div>
